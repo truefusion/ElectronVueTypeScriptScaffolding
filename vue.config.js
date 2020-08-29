59a24eb64f6e27c300bb3dd96935c9f157aef2be
Marco Ippolito <ippolito.marco@gmail.com>
2020-08-29 21:18:01 +0200

    vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,6 @@
+// https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#web-workers
+const WorkerPlugin = require('worker-plugin')
+
 module.exports = {
   // options...
   publicPath: '',
@@ -22,21 +25,8 @@ module.exports = {
         linux: {
           icon: './public/app.png'
         }
-      }
-    }
-  }
-}
-
-// https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#webpack-configuration
-module.exports = {
-  configureWebpack: {
-    // Configuration applied to all builds
-  },
-  pluginOptions: {
-    electronBuilder: {
-      chainWebpackMainProcess: (config) => {
-        // Chain webpack config for electron main process only
       },
+      // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#webpack-configuration
       chainWebpackRendererProcess: (config) => {
         // Chain webpack config for electron renderer process only
         // The following example will set IS_ELECTRON to true in your app
@@ -45,58 +35,24 @@ module.exports = {
           return args
         })
       },
-      // Use this to change the entrypoint of your app's main process
       mainProcessFile: 'src/background.js',
-      // Provide an array of files that, when changed, will recompile the main process and restart Electron
-      // Your main process file will be added by default
-      //mainProcessWatch: ['src/myFile1', 'src/myFile2'],
-      //mainProcessWatch: ['dist_electron/preload.js'],
-      // Provide a list of arguments that Electron will be launched with during "electron:serve",
-      // which can be accessed from the main process (src/background.js).
-      // Note that it is ignored when --debug flag is used with "electron:serve", as you must launch Electron yourself
-      // Command line args (excluding --debug, --dashboard, and --headless) are passed to Electron as well
-      //mainProcessArgs: ['--arg-name', 'arg-value']
-    }
-  }
-}
-
-// https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#typescript-options
-module.exports = {
-  pluginOptions: {
-    electronBuilder: {
-      // option: default // description
-      disableMainProcessTypescript: false, // Manually disable typescript plugin for main process. Enable if you want to use regular js for the main process (src/background.js by default).
-      mainProcessTypeChecking: false // Manually enable type checking during webpck bundling for background file.
-    }
-  }
-}
-
-// https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#preload-files
-module.exports = {
-  pluginOptions: {
-    electronBuilder: {
+      // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/configuration.html#typescript-options
+      disableMainProcessTypescript: false, // Manually disable typescript plugin for main process. Enable if you want to use regular js for the main process (src/background.js by default)
+      mainProcessTypeChecking: false, // Manually enable type checking during webpck bundling for background file.
+      // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#preload-files
       preload: 'src/preload.js',
-      //preload: 'dist_electron/preload.js',
-      // Or, for multiple preload files:
-      //preload: { preload: 'src/preload.js', otherPreload: 'src/preload2.js' }
-    }
-  }
-}
-
-// https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration
-module.exports = {
-  pluginOptions: {
-    electronBuilder: {
+      // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration
       nodeIntegration: true
+    },
+    // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#web-workers
+    configureWebpack: {
+      plugins: [new WorkerPlugin()]
     }
   }
-}
 
-// https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#web-workers
-const WorkerPlugin = require('worker-plugin')
-
-module.exports = {
+  // https://nklayman.github.io/vue-cli-plugin-electron-builder/guide/guide.html#web-workers
   configureWebpack: {
     plugins: [new WorkerPlugin()]
   }
+
 }

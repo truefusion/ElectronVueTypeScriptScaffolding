9e9c444422370c5d4f2bf2f4606ee182df5dfcee
Marco Ippolito <ippolito.marco@gmail.com>
2020-08-31 11:44:26 +0200

    src/background.ts src/store/modules/app.ts vue.config.js

--- src/store/modules/OLDapp.ts
+++ src/store/modules/OLDapp.ts
@@ -2,32 +2,24 @@ import Vue from 'vue'
 import { loadSettings, setSettings } from '@/services/electron-services/setting/setting'
 
 const TOGGLE_THEME = 'TOGGLE_THEME'
-const TOGGLE_LANG = 'TOGGLE_LANG'
 
 const stateRecord: App = loadSettings()
+
 
 const app = {
   state: {
     currentTheme: stateRecord.currentTheme || 'light',
-    //currentLang: stateRecord.currentLang || 'en',
   },
   mutations: {
     [TOGGLE_THEME](state: App, currentTheme: Theme) {
       state.currentTheme = currentTheme
     },
-    //[TOGGLE_LANG](state: App, currentLang: Language) {
-    //  state.currentLang = currentLang
-    //},
   },
   actions: {
     TOGGLE_THEME({ commit }: any, payload: App) {
       setSettings('settings.currentTheme', payload.currentTheme)
       commit(TOGGLE_THEME, payload.currentTheme)
     },
-    //TOGGLE_LANG({ commit }: any, payload: App) {
-    //  setSettings('settings.currentLang', payload.currentLang)
-    //  commit(TOGGLE_LANG, payload.currentLang)
-    //},
   },
 }
 

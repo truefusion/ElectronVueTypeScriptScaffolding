2d96265e966a8d3efb8396fc87b1f8a1bfc61352
Marco Ippolito <ippolito.marco@gmail.com>
2020-08-31 16:41:56 +0200

    src/services/electron-services/database/index.ts

--- src/services/electron-services/database/index.ts
+++ src/services/electron-services/database/index.ts
@@ -11,7 +11,7 @@ interface Schema {
     width: number
   }
   settings: {
-    currentLang: string
+    //currentLang: string
     currentTheme: string
   }
 }
@@ -58,9 +58,9 @@ class DB {
     if (this.db.get('settings.currentTheme').value() === 'purple') {
       this.db.set('settings.currentTheme', 'night').write()
     }
-    if (!this.db.has('settings.currentLang')) {
-      this.db.set('settings.currentLang', 'en').write()
-    }
+    //if (!this.db.has('settings.currentLang')) {
+      //this.db.set('settings.currentLang', 'en').write()
+    //}
   }
   // read() is to keep the data of the main process and the rendering process up to date.
   public read() {

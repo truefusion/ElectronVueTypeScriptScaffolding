619e604f04cde1de8f5d21d3301d7a763807fe1d
Marco Ippolito <ippolito.marco@gmail.com>
2020-08-31 20:13:48 +0200

    src/views/Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <Ipc />
+    <Ipc @setTheme="setTheme" />
   </div>
 </template>
 
@@ -20,19 +20,51 @@ import Ipc from '@/components/Ipc.vue'
 
 
 export default class Home extends Vue {
-  //@Getter('currentTheme') private getterTheme!: Theme
+  @Getter('currentTheme') private getterTheme!: Theme
 
-  //private setTheme(currentTheme: Theme): void {
-  //  const bodyTag: HTMLBodyElement | null = document.querySelector('body')
-  //  if (!bodyTag) {
-  //    return
-  //  }
-  //  bodyTag.className = currentTheme
-  //}
+  private setTheme(currentTheme: Theme): void {
+    const bodyTag: HTMLBodyElement | null = document.querySelector('body')
+    if (!bodyTag) {
+      return
+    }
+    bodyTag.className = currentTheme
+  }
 
-  //private created() {
+  private created() {
+    //console.log(this.getterTheme)
   //  this.setTheme(this.getterTheme)
-  //}
+  }
 }
 
 </script>
+
+<style lang="scss" scope>
+@import '~@/assets/scss/mixins.scss';
+.home-view {
+  height: 100%;
+  background-color: var(--color-bg-primary);
+  .topbar {
+    @include flex-space-between;
+    min-height: 60px;
+    padding: 0 16px;
+    border-bottom: 1px solid var(--color-border-default);
+  }
+  .right-topbar {
+    position: fixed;
+    left: 341px;
+    right: 0;
+    z-index: 3;
+    background: var(--color-bg-normal);
+  }
+  .rightbar {
+    margin-left: 81px;
+    height: 100%;
+    background-color: var(--color-bg-primary);
+  }
+  .right-content {
+    margin-left: 341px;
+    height: 100%;
+    background-color: var(--color-bg-primary);
+  }
+}
+</style>
